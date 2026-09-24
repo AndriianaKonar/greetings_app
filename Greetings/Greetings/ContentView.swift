b0562a01f3dbbb741314737e163d05ae15181523
KonarAndriiana <andriianakonar@gmail.com>
2026-09-24 16:20:46 +0200

    Add data model

--- Greetings/Greetings/ContentView.swift
+++ Greetings/Greetings/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let messages : [DataItemModel] = [
+        .init(text: "Hi, nice to meet you", color: .green),
+        .init(text: "Thats a great day", color: .blue),
+        .init(text: "To start our journey", color: .purple),
+        .init(text: "With beatifull greetings app", color: .pink),
+        .init(text: "Would you like to choose a language? ", color: .mint),
+    ]
+    
     var body: some View {
         VStack(alignment: .leading) {
-            TextView(text: "Hi, nice to meet you", color: .green)
-            TextView(text: "Thats a great day", color: .blue)
-            TextView(text: "To start our journey", color: .purple)
-            TextView(text: "With beatifull greetings app", color: .pink)
-            TextView(text: "Would you like to choose a language? ", color: .mint)
+            TextView(text: messages[0].text, color: messages[0].color)
+            TextView(text: messages[1].text, color: messages[1].color)
+            TextView(text: messages[2].text, color: messages[2].color)
+            TextView(text: messages[3].text, color: messages[3].color)
+            TextView(text: messages[4].text, color: messages[4].color)
         }
         .padding()
     }
---
+++ Greetings/Greetings/DataItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  DataItemModel.swift
+//  Greetings
+//
+//  Created by Andriiana Konar on 24/09/2026.
+//
+
+import Foundation
+import SwiftUI
+
+struct DataItemModel {
+    let text : String
+    let color : Color
+}
